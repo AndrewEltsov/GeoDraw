ccb92b365b2ecfb7415afcff9765e1032b544094
AndrewEltsov <andrei.eltzov@gmail.com>
2019-03-24 19:28:59 +0300

    Added ability to draw any type of feature

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 
@@ -10,46 +11,118 @@ import (
 )
 
 func main() {
+	var fileDest = flag.String("d", "borders.geojson", "Destination of file")
+	flag.Parse()
+	fmt.Println(*fileDest)
 	dc := gg.NewContext(1366, 1024)
-	dc.SetHexColor("fff")
 
 	dc.InvertY()
-	dc.Scale(6, 10)
+	dc.Scale(7, 12)
 
-	rawFeatureJSON, err := ioutil.ReadFile("borders.geojson")
+	rawFeatureJSON, err := ioutil.ReadFile(*fileDest)
 	if err != nil {
 		panic(err)
 	}
 
-	fc1, err := geojson.UnmarshalFeatureCollection(rawFeatureJSON)
+	fc, err := geojson.UnmarshalFeatureCollection(rawFeatureJSON)
 	if err != nil {
 		panic(err)
 	}
 
-	dc.SetLineWidth(1.0)
-	dc.SetRGB(1, 0, 0)
-	drawBackground("888", dc)
-	for _, multiPolygon := range fc1.Features[0].Geometry.MultiPolygon {
-		for _, polygon := range multiPolygon {
-			drawPolygon(polygon, dc)
-		}
-
+	err = drawMap(fc, dc)
+	if err != nil {
+		panic(err)
 	}
-
-	dc.SetHexColor("f00")
-	dc.Fill()
 	dc.SavePNG("out.png")
 
 }
 
-//
-func drawPolygon(polygon [][]float64, c *gg.Context) {
-	c.MoveTo(polygon[0][0], polygon[0][1])
-	for i := 0; i < len(polygon); i++ {
-		fmt.Println(polygon[i][0])
-		fmt.Println(polygon[i][1])
-		c.LineTo(polygon[i][0], polygon[i][1])
+func drawMap(fc *geojson.FeatureCollection, c *gg.Context) error {
+	drawBackground("000", c)
+	for _, feature := range fc.Features {
+		name, err := feature.PropertyString("name")
+		if err != nil {
+			return err
+		}
+		fmt.Println(name)
+		drawGeometry(feature.Geometry, c)
 	}
+	return nil
+}
+
+func drawGeometry(g *geojson.Geometry, c *gg.Context) {
+	if g.IsCollection() {
+		for _, geometry := range g.Geometries {
+			drawGeometry(geometry, c)
+		}
+		return
+	}
+	if g.IsLineString() {
+		drawLine(g.LineString, c)
+		return
+	}
+	if g.IsMultiLineString() {
+		for _, line := range g.MultiLineString {
+			drawLine(line, c)
+		}
+		return
+	}
+	if g.IsMultiPoint() {
+		for _, point := range g.MultiPoint {
+			drawPoint(point, c)
+		}
+		return
+	}
+	if g.IsMultiPolygon() {
+		for _, multiPolygon := range g.MultiPolygon {
+			for _, polygon := range multiPolygon {
+				drawPolygon(polygon, c)
+			}
+		}
+		return
+	}
+	if g.IsPoint() {
+		drawPoint(g.Point, c)
+		return
+	}
+	if g.IsPolygon() {
+		for _, polygon := range g.Polygon {
+			drawPolygon(polygon, c)
+		}
+		return
+	}
+}
+
+func drawLine(l [][]float64, c *gg.Context) {
+	c.SetHexColor("0f0")
+	c.MoveTo(l[0][0], l[0][1])
+	for _, p := range l {
+		c.LineTo(p[0], p[1])
+	}
+}
+
+func drawPoint(p []float64, c *gg.Context) {
+	c.SetHexColor("00f")
+	c.DrawPoint(p[0], p[1], 5)
+}
+
+func drawPolygon(polygon [][]float64, c *gg.Context) {
+	c.SetRGB(0, 0, 0)
+	c.SetLineWidth(1.0)
+	c.MoveTo(polygon[0][0], polygon[0][1])
+	var x float64
+	var y float64
+	for i := 0; i < len(polygon); i++ {
+		if polygon[i][0] < 0 {
+			x = polygon[i][0] + 180*2
+		} else {
+			x = polygon[i][0]
+		}
+		y = polygon[i][1]
+		c.LineTo(x, y)
+	}
+	c.SetHexColor("fff")
+	c.Fill()
 }
 
 func drawBackground(color string, c *gg.Context) {
